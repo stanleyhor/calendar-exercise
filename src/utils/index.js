6fe9fcbb328d717a1c7373c31083f14c2d1f4df0
Hor, Stanley <HorS@AE.com>
2017-04-18 10:07:15 -0700

    add options required by getDisplayDate

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
-const _HOUR_DISPLAY_MAP = [
-    '12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM',
-    '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM',
-]
+const dateOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
 
 /**
  * Given a list of events and a date, filter the events down to those that
